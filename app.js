c06bce3055892807759d2589c682f2d3cdd8ad73
safe731 <limonk829@gmail.com>
2026-06-13 22:40:19 +0600

    Refactor randomCode and generateLink functions

--- app.js
+++ app.js
@@ -1,38 +1,75 @@
-function randomCode(length = 7){
+function randomCode(length = 7) {
+  const chars =
+  "abcdefghijklmnopqrstuvwxyz0123456789";
 
-const chars =
-"abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
 
-let result = "";
+  for(let i=0;i<length;i++){
+    result += chars.charAt(
+      Math.floor(Math.random()*chars.length)
+    );
+  }
 
-for(let i=0;i<length;i++){
-
-result += chars.charAt(
-Math.floor(Math.random()*chars.length)
-);
-
-}
-
-return result;
-
+  return result;
 }
 
 async function generateLink(){
 
-let code =
-document.getElementById("customCode").value;
+  const url =
+  document.getElementById("url").value;
 
-if(!code){
+  const fileName =
+  document.getElementById("filename").value;
 
-code = randomCode();
+  const fileSize =
+  document.getElementById("filesize").value;
 
-}
+  const delay =
+  document.getElementById("delay").value;
 
-document.getElementById("result").innerHTML =
-`
-<p>
-https://safe731.github.io/go/?id=${code}
-</p>
-`;
+  let code =
+  document.getElementById("customCode").value;
+
+  if(!code){
+    code = randomCode();
+  }
+
+  const response = await fetch(
+    "https://s.safe731-shortlink-api.workers.dev/create",
+    {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        code,
+        url,
+        fileName,
+        fileSize,
+        delay
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  if(data.success){
+
+    document.getElementById("result").innerHTML = `
+      <p>
+      Generated Link:
+      </p>
+
+      <input
+      value="https://safe731.github.io/go/?id=${code}"
+      readonly>
+
+    `;
+
+  }else{
+
+    alert("Error Creating Link");
+
+  }
 
 }
